68ddbea5047eacae56838aa110fd6762d4dc6398
molexandev01~ <molexandev@gmail.com>
2025-02-26 19:56:45 +0200

    added services

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-import { createRoot } from "react-dom/client";
-import App from "./components/app/App";
-import "./style/style.scss";
+import { createRoot } from 'react-dom/client';
+import App from './components/app/App';
+import MarvelService from './services/MarvelService';
+import './style/style.scss';
 
-const container = document.getElementById("root");
+const marvelService = new MarvelService();
+
+// marvelService.getAllCharacters().then((res) => console.log(res));
+marvelService.getCharacter(1011052).then((res) => console.log(res));
+
+const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App />);
