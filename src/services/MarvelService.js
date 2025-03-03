ca04db182905d0ad4aa619bc1385b5d5339641e2
molexandev01~ <molexandev@gmail.com>
2025-03-03 20:29:28 +0200

    added data transformation and random character component

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -13,16 +13,34 @@ class MarvelService {
       return await res.json();
    };
 
-   getAllCharacters = () => {
-      return this.getResource(
+   getAllCharacters = async () => {
+      const res = await this.getResource(
          `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
       );
+      return res.data.results.map(this._transformCharacter);
    };
 
-   getCharacter = (id) => {
-      return this.getResource(
+   getCharacter = async (id) => {
+      const res = await this.getResource(
          `${this._apiBase}characters/${id}?${this._apiKey}`
       );
+      return this._transformCharacter(res.data.results[0]);
+   };
+
+   _transformCharacter = (char) => {
+      let newDescr = '';
+      if (char.description.length === 0) {
+         newDescr = 'Sorry! Description not loaded';
+      } else if (char.description.length > 150) {
+         newDescr = char.description.slice(0, 150) + '...';
+      }
+      return {
+         name: char.name,
+         description: newDescr,
+         thumbnail: char.thumbnail.path + '.' + char.thumbnail.extention,
+         homepage: char.urls[0].url,
+         wiki: char.urls[1].url,
+      };
    };
 }
 
