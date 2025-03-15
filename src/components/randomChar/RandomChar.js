4f9a2dab10223efbebfd02f1dea670e1f9be6667
molexandev01~ <molexandev@gmail.com>
2025-03-15 17:54:30 +0200

    charInfo and randumchar have been reworked into a functional component

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,95 +1,85 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
+
 import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
 
-class RandomChar extends Component {
-   state = {
-      char: {},
-      loading: true,
-      error: false,
+const RandomChar = () => {
+   const [char, setChar] = useState(null);
+   const [loading, setLoading] = useState(true);
+   const [error, setError] = useState(false);
+
+   const marvelService = new MarvelService();
+
+   useEffect(() => {
+      updateChar();
+      const timerId = setInterval(updateChar, 60000);
+
+      return () => {
+         clearInterval(timerId);
+      };
+   }, []);
+
+   const onCharLoaded = (char) => {
+      setLoading(false);
+      setChar(char);
    };
 
-   marvelService = new MarvelService();
-
-   componentDidMount() {
-      // this.foo.bar = 0;
-      this.updateChar();
-      // this.timerId = setInterval(this.updateChar, 3000);
-      console.log('CDM');
-   }
-
-   componentDidUpdate() {
-      console.log('CDU');
-   }
-
-   componentWillUnmount() {
-      // clearInterval(this.timerId);
-      console.log('CWU');
-   }
-
-   onCharLoaded = (char) => {
-      this.setState({ char, loading: false });
+   const onCharLoading = () => {
+      setLoading(true);
    };
 
-   onCharLoading = () => {
-      this.setState({
-         loading: true,
-      });
+   const onError = () => {
+      setError(true);
+      setLoading(false);
    };
 
-   onError = () => {
-      this.setState({ loading: false, error: true });
+   const updateChar = () => {
+      const id = Math.floor(Math.random() * (1011400 - 1011000)) + 1011000;
+      onCharLoading();
+      marvelService.getCharacter(id).then(onCharLoaded).catch(onError);
    };
 
-   updateChar = () => {
-      const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-      this.onCharLoading();
-      this.marvelService
-         .getCharacter(id)
-         .then(this.onCharLoaded)
-         .catch(this.onError);
-   };
+   const errorMessage = error ? <ErrorMessage /> : null;
+   const spinner = loading ? <Spinner /> : null;
+   const content = !(loading || error || !char) ? <View char={char} /> : null;
 
-   render() {
-      const { char, loading, error } = this.state;
-      const errorMessage = error ? <ErrorMessage /> : null;
-      const spinner = loading ? <Spinner /> : null;
-      const content = !(loading || error) ? <View char={char} /> : null;
-
-      return (
-         <div className="randomchar">
-            {errorMessage}
-            {spinner}
-            {content}
-            <div className="randomchar__static">
-               <p className="randomchar__title">
-                  Random character for today!
-                  <br />
-                  Do you want to get to know him better?
-               </p>
-               <p className="randomchar__title">Or choose another one</p>
-               <button
-                  className="button button__main"
-                  onClick={this.updateChar}
-               >
-                  <div className="inner">try it</div>
-               </button>
-               <img
-                  src={mjolnir}
-                  alt="mjolnir"
-                  className="randomchar__decoration"
-               />
-            </div>
+   return (
+      <div className="randomchar">
+         {errorMessage}
+         {spinner}
+         {content}
+         <div className="randomchar__static">
+            <p className="randomchar__title">
+               Random character for today!
+               <br />
+               Do you want to get to know him better?
+            </p>
+            <p className="randomchar__title">Or choose another one</p>
+            <button onClick={updateChar} className="button button__main">
+               <div className="inner">try it</div>
+            </button>
+            <img
+               src={mjolnir}
+               alt="mjolnir"
+               className="randomchar__decoration"
+            />
          </div>
-      );
-   }
-}
+      </div>
+   );
+};
 
 const View = ({ char }) => {
    const { name, description, thumbnail, homepage, wiki } = char;
+   let imgStyle = { objectFit: 'cover' };
+   if (
+      thumbnail ===
+      'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
+   ) {
+      imgStyle = { objectFit: 'contain' };
+   }
 
    return (
       <div className="randomchar__block">
@@ -97,6 +87,7 @@ const View = ({ char }) => {
             src={thumbnail}
             alt="Random character"
             className="randomchar__img"
+            style={imgStyle}
          />
          <div className="randomchar__info">
             <p className="randomchar__name">{name}</p>
